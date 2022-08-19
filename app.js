0271b2cf4715b0120c6b64fba79d14890c2af358
Jimena Miguez <jimeblau@gmail.com>
2022-08-19 15:30:58 +0200

    Finish events

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 new Vue({
-  el: '#vue-app',
+  el: "#vue-app",
   data: {
-    name: 'Jime',
-    job: 'ninja',
-    website: 'https://github.com/JimeBlue',
-    websiteTag:
-      '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>',
+    age: 25,
+    // These variables are used in the updateXY method below
+    x: 0,
+    y: 0,
   },
-
+  // EXPLANATION: the methods add and subtract are called from the html file with the on click event.
+  // They accept parameters that are passed when the methods are called
   methods: {
-    greet(time) {
-      return `god ${time} ${this.name}`;
+    add(inc) {
+      this.age += inc;
+    },
+    subtract(dec) {
+      this.age -= dec;
+    },
+    // EXPLANATION: this method is fired by the on mouse move event in html. Here we have access to
+    // the event object that comes by default with the mouse event listener
+    updateXY(event) {
+      // offsetX and offsetY are properties of the event object
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     },
   },
 });
